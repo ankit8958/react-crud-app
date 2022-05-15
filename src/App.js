533fd5c768d6f18201244053f9c270ee389231dd
Ankit Gupta <ankit.gupta@ankitr389.successivetech.com>
2022-05-15 22:42:30 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import {Routes,Route} from 'react-router-dom';
+import Home from './components/Pages/Home';
+import View from "./components/student/View";
+import Edit from "./components/student/Edit";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Routes>
+        <Route exact path="/" element={<Home/>}/>
+        <Route exact path="/view/:id" element={<View/>} />
+          <Route exact path="/edit/:id" element={<Edit/>} />
+      </Routes>
+    </>
   );
 }
 
